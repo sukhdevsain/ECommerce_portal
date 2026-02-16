2aa7228e6e89dfa26c2b0b6a95a008086d323aa1
Sukhdevsain <sukhdevsainittechxpert@gmail.com>
2026-02-16 20:33:48 +0530

    new

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -6,7 +6,7 @@
 
 @section('content')
 <div class="container-fluid bg-light p-5">
-    <h1 class="text-center text-secondary"><i class="fa-solid fa-layer-group"></i> {{ $category->c_name }}</h1>
+    <h1 class="text-center  text-secondary"><i class="fa-solid fa-layer-group"></i> {{ $category->c_name }}</h1>
 </div>
 
 <!-- Products -->
